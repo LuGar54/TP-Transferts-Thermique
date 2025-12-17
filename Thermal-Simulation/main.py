a0aa56d855c56ee4fe0b304f097488b973e87644
Andrew Forrester <andrew.forrester.1@ulaval.ca>
2025-12-17 10:23:34 -0500

    clean up main

--- Thermal-Simulation/main.py
+++ Thermal-Simulation/main.py
@@ -5,19 +5,20 @@ from MatriceTransfert import Lcm
 from Capacitances import vol_cells as vols
 from Capacitances import C_cells as C
 
+
 def main():
-    R12, R23, R34, R45, R56, R17, R28, R39, R410, R511, R612 = sp.symbols('R12 R23 R34 R45 R56 R17 R28 R39 R410 R511 R612')
+    R_horiz = [sp.Symbol(f'R{i}{i+1}') for i in range(1, 6)]
+    R_vert = [sp.Symbol(f'R{i}{i+6}') for i in range(1, 7)]
 
-    C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12 = sp.symbols('C1 C2 C3 C4 C5 C6 C7 C8 C9 C10 C11 C12')
-
-    cmat = sp.diag(C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12).inv()
+    C = sp.symbols('C1:13')
+    cmat = sp.diag(*[1/c for c in C])
     
-    model = Lcm([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-    model.series([0, 1, 2, 3, 4, 5], [R12, R23, R34, R45, R56])
-    for i in range(6):
-        model.series([i, i+6], [locals()[f'R{i+1}{i+7}']])
+    model = Lcm(list(range(1, 13)))
+    model.series(list(range(6)), R_horiz)
+    for i, r_symbol in zip(range(6), R_vert):
+        model.series([i, i+6], [r_symbol])
 
-    # matrice 
+     
     transfer_mat = cmat @ model.matrix
     transfer_mat.simplify()
     # sp.pprint(cmat)
