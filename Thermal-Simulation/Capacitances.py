0da3f375881e03cef89707f0ad8141f79ff0be79
Ludovic Garon <ludovicgaron54@gmail.com>
2025-12-17 08:44:03 -0500

    centralized material properties

--- Thermal-Simulation/Capacitances.py
+++ Thermal-Simulation/Capacitances.py
@@ -1,13 +1,14 @@
 import numpy as np
+from ProprietesMats import *
 
 
 def volume_concrete(is_bou:bool):
     # plancher
-    vol_floor = tot_width * p_len * thickness
+    vol_floor = tot_width * p_len * thickness_conc
     # murs
-    vol_walls = 2 * (tot_height * p_len * thickness) 
+    vol_walls = 2 * (tot_height * p_len * thickness_conc) 
     if is_bou:
-        vol_walls += (tot_height * tot_width * thickness)
+        vol_walls += (tot_height * tot_width * thickness_conc)
     return vol_floor + vol_walls
 
 def get_capacitance(rho, cp, is_bou:bool):
@@ -15,19 +16,7 @@ def get_capacitance(rho, cp, is_bou:bool):
     C_concrete = rho * cp * vol_concrete
     return C_concrete
 
-    
-# Dimension totale
-tot_len = 26.1 
-tot_width = 3.7
-tot_height = 1.7
-thickness = 0.4
-p_len = tot_len/6
-
-# Propriétés du béton
-rho = 2400  # kg/m3
-cp = 880    # J/kgK
-
-isbout_vec = [True, False, False, False, False, True]
 vol_cells = [volume_concrete(i) for i in isbout_vec]
 C_cells = [get_capacitance(rho, cp, i) for i in isbout_vec]
 
+print(C_cells)
---
+++ Thermal-Simulation/ProprietesMats.py
@@ -0,0 +1,17 @@
+# Dimension totale
+tot_len = 26.1 
+tot_width = 3.7
+tot_height = 1.7
+thickness_conc = 0.4
+p_len = tot_len/6
+
+# Propriétés du béton
+rho = 2400  # kg/m3
+cp = 880    # J/kgK
+
+# Propriétés des cellules
+isbout_vec = [True, False, False, False, False, True]
+
+# Propriétés de l'isolant
+k_insul = 0.02  # W/mK
+thickness_insul = 0.1  # m
